e2280a5987c5ddf6041d06fbf915aa7a1d7f41ea
Efutei <hirofunafumi@gmail.com>
2017-12-16 13:33:20 +0900

    add spawnBanana

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ var SCREEN_WIDTH  = 465;
 var SCREEN_HEIGHT = 665;
 var moveSpeed = 3;
 var stopScroll = false;
+var karinCatchable = true;
 var KARIN_START_X = SCREEN_WIDTH / 2 - 50;
 var KARIN_START_Y = SCREEN_HEIGHT / 2 + 80;
 
@@ -28,9 +29,33 @@ phina.define('MainScene', {
     this.bg1.x = -SCREEN_WIDTH*2 - 3;
 
     this.karin = Karin().addChildTo(this);
+    this.spawnBanana();
+
+    this.bananas = DisplayElement().addChildTo(this);
   },
   update: function(app){
-
+    if(!stopScroll){
+      this.bg0.move();
+      this.bg1.move();
+      this.karin.hopping();
+    }
+    var p = app.pointer;
+    if(p.getPointingStart()){
+      this.karin.nextCatch = true;
+    }
+  },
+  spawnBanana: function(){
+    this.tweener
+    .wait(500)
+    .call(function(){
+      console.log(this.target);
+      var rnd = Random.randint(0, 5)
+      if(rnd == 0){
+        Banana().addChildTo(this.target.bananas);
+      }
+    })
+    .wait(500)
+    .setLoop(true);
   }
 });
 
@@ -42,7 +67,6 @@ phina.define('Bg', {
     this.y = SCREEN_HEIGHT / 2;
   },
   update: function(){
-    this.move();
   },
   move: function(){
     this.x -= moveSpeed;
@@ -58,25 +82,73 @@ phina.define('Bg', {
 phina.define('Karin', {
   superClass: 'Sprite',
   init: function(){
-    this.superInit('tongUpKarin', 179 * 1.3, 188 * 1.3);
+    this.superInit('tongUpKarin', 230 * 1.3, 188 * 1.3);
     this.x = KARIN_START_X;
     this.y = KARIN_START_Y;
     this.scaleX *= -1;
+    this.nextCatch = false;
   },
   update: function(){
-    this.hopping();
+    if(this.nextCatch && karinCatchable){
+      this.nextCatch = false;
+      this.catchBanana();
+    }
   },
   hopping: function(){
     this.x -= moveSpeed;
     this.tweener
+    .call(function(){
+      karinCatchable = false
+    })
     .to({
-      x: KARIN_START_X + 15,
+      x: KARIN_START_X + 10,
       y: KARIN_START_Y - 20
     },250,"swing")
     .to({
+      x: KARIN_START_X + 10,
       y: KARIN_START_Y
-    },250,"swing")
+    },180,"swing")
+    .call(function(){
+      karinCatchable = true
+    })
+    .wait(150)
     .play();
+  },
+  catchBanana: function(){
+    console.log("hoge");
+    stopScroll = true;
+    karinCatchable = false;
+    this.tweener
+    .clear()
+    .set({
+      y: KARIN_START_Y
+    })
+    .rotateBy(10, 100, "swing")
+    .rotateBy(-10, 100, "swing")
+    .wait(300)
+    .call(function(){
+      stopScroll = false;
+      karinCatchable = true;
+    });
+
+  }
+});
+
+phina.define('Banana', {
+  superClass: 'Sprite',
+  init: function(){
+    this.superInit('banana', 100, 100);
+    this.x = SCREEN_WIDTH;
+    this.y = KARIN_START_Y + 98;
+  },
+  update: function(){
+    this.move();
+  },
+  move: function(){
+    this.x -= moveSpeed;
+  },
+  checkOutOfWindow: function(){
+    return this.x < -SCREEN_WIDTH;
   }
 });
 
