7bacc968c425d51503ce23bdce375a9e6983b7d1
Efutei <hirofunafumi@gmail.com>
2017-12-18 06:08:41 +0900

    change tweetMessage

--- main.js
+++ main.js
@@ -414,7 +414,7 @@ phina.define('ResultScene', {
     }
 
     this.shareButton.onclick = function() {
-      var text = 'Score: {0}\n{1}'.format(params.score, "バナナには勝てなかったよ...\n");
+      var text = 'Score: {0}\n{1}\n{2}\n'.format(params.score, this.parent.rankingLabel.text, "バナナには勝てなかったよ...");
       var url = phina.social.Twitter.createURL({
         text: text,
         hashtags: params.hashtags,
